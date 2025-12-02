9d52bd7498874457dbb221a9ac3b978395c23deb
Getuar333 <getuar.gaming@gmail.com>
2025-12-02 17:18:16 +0100

    Update summary comment for Register.cshtml.cs

--- RestaurantSystem/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ RestaurantSystem/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -46,8 +46,8 @@ namespace RestaurantSystem.Areas.Identity.Pages.Account
         }
 
         /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
+        ///     This API supports the default ASP.NET Core Identity UI infrastructure and is not intended to be used
+        ///     directly from your code. And this API may change or be removed in future releases.
         /// </summary>
         [BindProperty]
         public InputModel Input { get; set; }
